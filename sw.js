8d9ada210ab8d3c8751185bf164c0d5883e49325
Nada <158790725+NadaBaiomy@users.noreply.github.com>
2026-05-15 23:21:49 +0300

    Implement service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE = 'fc-v1';
+var URLS = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) {
+    return c.addAll(URLS);
+  }));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        return caches.open(CACHE).then(function(c) {
+          if (e.request.url.indexOf('supabase') < 0) {
+            c.put(e.request, res.clone());
+          }
+          return res;
+        });
+      });
+    }).catch(function() {
+      return caches.match('/index.html');
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(ks) {
+    return Promise.all(ks.filter(function(k) {
+      return k !== CACHE;
+    }).map(function(k) {
+      return caches.delete(k);
+    }));
+  }));
+  self.clients.claim();
+});
